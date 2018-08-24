dbe78845d5e9e704963913b261d9e46835cbc3c1
Gonzalo Munoz <gonzaloan.munoz@gmail.com>
2018-08-23 23:45:46 -0300

    Exceptions

---
+++ Scripts/Ejercicios 2_9 - descomposicion.py
@@ -0,0 +1,11 @@
+﻿import sys
+
+if(len(sys.argv) == 2 and sys.argv[1].isdigit() and int(sys.argv[1])>0):
+	largonum = len(sys.argv[1])
+	formateo = "{:0" + str(largonum) + "d}"
+	for indice,num in enumerate(sys.argv[1]):
+		ceros=largonum-indice-1
+		resultado = num + '0'*ceros
+		print(formateo.format(int(resultado)))
+else:
+	print("Se debe introducir un parámetro, que debe ser un entero positivo")
---
+++ Scripts/Ejercicios 2_9 - tabla.py
@@ -0,0 +1,11 @@
+﻿import sys
+
+if(len(sys.argv) == 3 and sys.argv[1].isdigit() and sys.argv[2].isdigit() and (int(sys.argv[1]) > 0 and int(sys.argv[1]) < 10) and (int(sys.argv[2]) > 0 and int(sys.argv[2]) < 10)):
+	filas = int(sys.argv[1])
+	columnas = int( sys.argv[2])
+	for i in range(filas):
+		for x in range(columnas):
+			print(" * ", end='')
+		print("")
+else:
+	print("Deben ingresarse dos valores como parámetros y ser números entre 1 y 9")
